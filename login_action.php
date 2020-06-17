cfe28ec7032fb9e1fd61b81b78fd5c12180ba0f0
workaahmed16@gmail.com <workaahmed16@gmail.com>
2020-06-17 14:37:41 -0400

    Now checks database for whether usename exists successfully

--- login_action.php
+++ login_action.php
@@ -12,6 +12,48 @@ if (isset($_POST['login_submit'])) {
 	if (empty($user) || empty($pass)) {
 		header("Location: login.php?error=emptyfields");
 		exit();
+	}else{
+
+		//SQL statement checking if the userName is already in the database. 
+		$sql = "SELECT * FROM users WHERE userName=?;";
+
+		//Connecting to MySql database
+		$statement = mysqli_stmt_init($con);
+		
+
+		//Error checking if statement to determine whether connection to db was succcessful.
+		if (!mysqli_stmt_prepare($statement, $sql)) {
+			header("Location: login.php?error=sqlerror");
+			exit();
+		}else{
+			mysqli_stmt_bind_param($statement, "s", $user);
+			mysqli_stmt_execute($statement);
+			$result = mysqli_stmt_get_result($statement);
+			
+
+			if ($row = mysqli_fetch_assoc($result)) {
+				$pwdCheck = password_verify($pass, $row['password']);
+				if ($pwdCheck == false) {
+					header("Location: login.php?error=wrongpassword");
+					exit();
+				}else if ($pwdCheck == true){
+					//Beginning users session. 
+
+					session_start();
+					$_SESSION['userName'] = $row['userName'];
+					$_SESSION['userName'] = $row['userName'];
+
+					header("Location: home.php?login=success");
+					exit();
+				}else{
+					header("Location: login.php?error=wrongpassword");
+					exit();
+				}
+			}else{
+				header("Location: login.php?error=nouser");
+				exit();
+			}
+		}
 	}
 
 }else{
